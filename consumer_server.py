ce9b8aac12f4ff0e236ef5e6d5c4fda748c0fb89
stoneheart <kz9892@gmail.com>
2020-05-05 22:54:07 -0700

    implement consumer_server.py

---
+++ consumer_server.py
@@ -0,0 +1,69 @@
+from confluent_kafka import Consumer, OFFSET_BEGINNING
+import time
+
+import logging
+import logging.config
+
+logging.config.fileConfig('logging.ini')
+logger = logging.getLogger(__name__)
+
+class KafkaConsumer:
+
+    def __init__(
+        self,
+        topic_name,
+        group_id,
+        offset_earliest=False,
+        consume_timeout=1.0
+    ):
+        self.topic_name = topic_name
+        self.offset_earliest = offset_earliest
+        self.consume_timeout = consume_timeout
+
+        # create consumer with assigned properties
+        self.broker_properties = {
+            'bootstrap.servers': 'PLAINTEXT://localhost:9092',
+            'group.id': group_id
+        }
+        if offset_earliest:
+            self.broker_properties['auto.offset.reset'] = 'earliest'
+
+        logger.info(f'create consumer with conf {self.broker_properties}')
+        self.consumer = Consumer(self.broker_properties)
+        self.consumer.subscribe([self.topic_name], on_assign=self.on_assign)
+
+    def on_assign(self, consumer, partitions):
+        if self.offset_earliest:
+            for partition in partitions:
+                partition.offset = OFFSET_BEGINNING
+        consumer.assign(partitions)
+
+    def consume(self):
+        try:
+            message = self.consumer.poll(self.consume_timeout)
+        except Exception as e:
+            logger.exception(f'exception from consumer {e}')
+
+        if message is None:
+            logger.debug('no message received by consumer')
+        elif message.error():
+            logger.debug(f'error from consumer {message.error()}')
+        else:
+            logger.info(f'consumed message {message.key()}: {message.value()}')
+
+    def close(self):
+        logger.info('closing consumer')
+        self.consumer.close()
+
+def run():
+    consumer = KafkaConsumer('police.department.calls', 'test-consumer')
+    try:
+        while True:
+            consumer.consume()
+            time.sleep(1)
+    except KeyboardInterrupt as e:
+        logger.info('shutting down consumer')
+        consumer.close()
+
+if __name__ == '__main__':
+    run()
--- start_kafka.sh
+++ start_kafka.sh
@@ -1,5 +1,6 @@
 echo 'Start Zookeeper server as daemon'
 $KAFKA_HOME/bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
+sleep 2
 
 echo 'Start Kafka server as daemon'
 $KAFKA_HOME/bin/kafka-server-start.sh -daemon config/server.properties
